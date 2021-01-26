ace8838c6fab10e348245d07d317ee531fbf12a6
Filip Krawczyk <fkrawczyk77@gmail.com>
2021-01-26 18:40:45 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+})
+
+var name;
+
+io.on('connection', (socket) => {
+	console.log("New user connected");
+
+	socket.on('joining msg', (n) => {
+		name = n;
+		io.emit('chat message', `---${name} joined the chat---`);
+	})
+
+	socket.on('disconnect', () => {
+		console.log(`${name} disconnected`);
+	})
+
+	socket.on('chat message', (msg) => {
+		socket.broadcast.emit('chat message', msg);
+	})
+});
+
+server.listen(3002, () => {
+	console.log('Server listening on :3002');
+});
